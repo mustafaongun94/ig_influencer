0528553f5c901b00ed1a2ebb50be6c569b611f7c
Mustafa Öngün <106812275+mustafaongun94@users.noreply.github.com>
2024-05-21 03:01:00 +0300

    Update scraper.py

--- ig_scraper/scraper.py
+++ ig_scraper/scraper.py
@@ -1,6 +1,9 @@
 import os
+import random
+
 import instaloader
 import boto3
+from botocore.exceptions import NoCredentialsError
 from django.conf import settings
 from datetime import datetime
 from .models import Influencer, Post, Story
@@ -20,15 +23,30 @@ s3_client = boto3.client(
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
 )
 
-def download_image_to_s3(img_url, bucket_name, s3_path):
+
+def download_media_to_s3(media_url, bucket_name, s3_path):
     try:
-        img_data = requests.get(img_url).content
-        s3_client.put_object(Bucket=bucket_name, Key=s3_path, Body=img_data)
+        response = requests.get(media_url)
+        content_type = response.headers['Content-Type']
+
+        if 'image' in content_type:
+            media_type = 'image'
+        elif 'video' in content_type:
+            media_type = 'video'
+        else:
+            print("Unsupported media type")
+            return None
+
+        s3_client.put_object(Bucket=bucket_name, Key=s3_path, Body=response.content, ContentType=content_type)
         s3_url = f"https://{bucket_name}.s3.{settings.AWS_S3_REGION_NAME}.amazonaws.com/{s3_path}"
-        print(f"Image successfully uploaded to {s3_url}")
+        print(f"{media_type.capitalize()} successfully uploaded to {s3_url}")
         return s3_url
+
+    except NoCredentialsError:
+        print("Credentials not available")
+        return None
     except Exception as e:
-        print(f"Failed to upload image to S3: {e}")
+        print(f"Failed to upload media to S3: {e}")
         return None
 
 def fetch_influencer_data(username):
@@ -44,38 +62,84 @@ def fetch_influencer_data(username):
             defaults={'full_name': profile.full_name}
         )
 
-        posts = profile.get_posts()
-        for post in posts:
+        for post in profile.get_posts():
             time.sleep(5)
-            s3_path = f"influencers/{username}/posts/{post.shortcode}.jpg"
-            media_url = download_image_to_s3(post.url, settings.AWS_STORAGE_BUCKET_NAME, s3_path)
-            
-            Post.objects.update_or_create(
+
+            if post.is_video:
+                media_type = "videos"
+                media_url = post.video_url
+                extension = "mp4"
+            else:
+                media_type = "posts"
+                media_url = post.url
+                extension = "jpg"
+
+            s3_path = f"influencers/{profile.username}/{media_type}/{post.shortcode}.{extension}"
+            s3_media_url = download_media_to_s3(media_url, settings.AWS_STORAGE_BUCKET_NAME, s3_path)
+
+            if post.typename == "GraphSidecar":
+                for sidecar in post.get_sidecar_nodes():
+                    if sidecar.is_video:
+                        media_type = "videos"
+                        media_url = sidecar.video_url
+                        extension = "mp4"
+                    else:
+                        media_type = "posts"
+                        media_url = sidecar.display_url
+                        extension = "jpg"
+                    sidecar_id = post.shortcode + str(random.randint(1,100000))+ str(random.randint(1,10000))
+                    s3_path = f"influencers/{profile.username}/{media_type}/{sidecar_id}.{extension}"
+                    s3_media_url = download_media_to_s3(media_url, settings.AWS_STORAGE_BUCKET_NAME, s3_path)
+
+                    Post.objects.update_or_create(
+                        post_id=sidecar_id,
+                        defaults={
+                            'influencer': influencer,
+                            'caption': post.caption,
+                            'media_url': s3_media_url,
+                            'like_count': post.likes,
+                            'comment_count': post.comments,
+                            'created_at': datetime.fromtimestamp(post.date_utc.timestamp()),
+                            'is_video': sidecar.is_video
+                        }
+                    )
+            else:
+                Post.objects.update_or_create(
                 post_id=post.shortcode,
                 defaults={
                     'influencer': influencer,
                     'caption': post.caption,
-                    'media_url': media_url,
+                    'media_url': s3_media_url,
                     'like_count': post.likes,
                     'comment_count': post.comments,
-                    'created_at': datetime.fromtimestamp(post.date_utc.timestamp())
+                    'created_at': datetime.fromtimestamp(post.date_utc.timestamp()),
+                    'is_video': post.is_video
                 }
-            )
-
+                )
         stories = L.get_stories(userids=[profile.userid])
         for story in stories:
             time.sleep(5)
             items = story.get_items()
             for item in items:
-                s3_path = f"influencers/{username}/stories/{item.mediaid}.jpg"
-                media_url = download_image_to_s3(item.url, settings.AWS_STORAGE_BUCKET_NAME, s3_path)
-                
+                if item.is_video:
+                    media_type = "videos"
+                    media_url = item.video_url
+                    extension = "mp4"
+                else:
+                    media_type = "stories"
+                    media_url = item.url
+                    extension = "jpg"
+
+                s3_path = f"influencers/{username}/{media_type}/{item.mediaid}.{extension}"
+                s3_media_url = download_media_to_s3(media_url, settings.AWS_STORAGE_BUCKET_NAME, s3_path)
+
                 Story.objects.update_or_create(
                     story_id=item.mediaid,
                     defaults={
                         'influencer': influencer,
-                        'media_url': media_url,
-                        'created_at': datetime.fromtimestamp(item.date_utc.timestamp())
+                        'media_url': s3_media_url,
+                        'created_at': datetime.fromtimestamp(item.date_utc.timestamp()),
+                        'is_video': item.is_video
                     }
                 )
     except Exception as e:
